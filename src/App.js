224bb684c5667c2f1e577c1ec3ebdee3db5193ce
HARSHVARDHAN BARETH <137777618+HARSHVARDHAN-BARETH@users.noreply.github.com>
2025-06-15 10:57:38 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import {TodoWrapper} from "./component/TodoWrapper"
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <TodoWrapper/>
+    </div>
+  );
+}
+
+export default App;
